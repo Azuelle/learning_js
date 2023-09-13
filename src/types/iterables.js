fe8b4cf63aa78e11502314ccaacb23ea69cfaf2c
Azuelle <tjk@live.cn>
2023-09-13 21:43:22 +0800

    Implement `range` as an iterable

--- src/types/iterables.js
+++ src/types/iterables.js
@@ -1,8 +1,24 @@
 const prompt = require("prompt-sync")();
 
-function range(l, r) {
-    this[Symbol.iterator] = () => {
-    };
+function range(l, r, step = 1) {
+    return {
+        l: l,
+        r: r,
+        step: step,
+        [Symbol.iterator]: () => ({
+            current: l,
+            last: r,
+            step: step,
+            next() {
+                if (this.current < this.last) {
+                    let ret = { done: false, value: this.current };
+                    this.current += this.step;
+                    return ret;
+                }
+                else return { done: true };
+            }
+        })
+    }
 }
 
-console.log(Array.from(range(1, 5)));
+console.log(Array.from(range(1, 12, 2)));
